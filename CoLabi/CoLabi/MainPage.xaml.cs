ddcf41cb709f0c152ac9b404b880755291c5e98e
renia <renia@DESKTOP-K2K84P4>
2017-03-05 23:24:15 +0100

    Add project files.

---
+++ CoLabi/CoLabi/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CoLabi
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
